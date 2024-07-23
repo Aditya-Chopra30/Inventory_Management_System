1edf18adc1bf08a797cce483005fe3225bb0deb1
Aditya Chopra <110564061+Aditya-Chopra30@users.noreply.github.com>
2024-07-23 18:43:35 +0530

    Update Raw_Program.java

--- Raw_Program.java
+++ Raw_Program.java
@@ -1,1 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Product {
+    private String name;
+    private String category;
+    private int quantity;
+
+    public Product(String name, String category, int quantity) {
+        this.name = name;
+        this.category = category;
+        this.quantity = quantity;
+    }
 
